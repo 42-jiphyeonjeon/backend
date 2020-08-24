174704b4d1ec7657379eb39ed40bc0e49b6e61fe
hygoni <9hrack@naver.com>
2020-08-25 00:19:13 +0900

    [ADD] 예외 클래스 및 예외 핸들러 미들웨어 추가
    예외 클래스, 예외 핸들러 미들웨어를 추가했고 사용법은 다음을 참고하세요.
    https://codeburst.io/better-error-handling-in-express-js-b118fc29e9c7

---
+++ src/exceptions/GeneralError.js
@@ -0,0 +1,19 @@
+class GeneralError extends Error {
+  constructor(message) {
+    super();
+    this.message = message;
+  }
+
+  getCode() {
+	const BadRequest = require('./BadRequest');
+	const NotFound = require('./NotFound');
+    if (this instanceof BadRequest) {
+      return 400;
+    } if (this instanceof NotFound) {
+      return 404;
+    }
+    return 500;
+  }
+}
+
+module.exports = GeneralError;
