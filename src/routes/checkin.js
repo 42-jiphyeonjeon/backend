9cf58d20a82ef22893f8883a31e1c81383884604
congcoding <congcoding@gmail.com>
2020-08-26 21:06:25 +0900

    [UPDATE] 반납 상세보기 수정

--- src/routes/checkin.js
+++ src/routes/checkin.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const { BookTiger } = require('../models');
+const {
+  BookTiger, BookInfo, Checkout, UserAccount,
+} = require('../models');
 
 const router = express.Router();
 
-router.get('/:bookTigerId', (req, res) => {
-  const { bookTigerId } = req.params;
-  console.log(bookTigerId);
-  BookTiger.findByPk(bookTigerId, {
-    include: [{ all: true }],
+router.get('/:checkoutId', (req, res) => {
+  const { checkoutId } = req.params;
+  Checkout.findByPk(checkoutId, {
+    include: [{ all: true }, { model: BookTiger, include: [BookInfo] }],
   }).then((result) => { res.json(result); });
 });
+
+// router.get('/:bookTigerId', (req, res) => {
+//   const { bookTigerId } = req.params;
+//   BookTiger.findByPk(bookTigerId, {
+//     include: [BookInfo, { model: Checkout, include: [UserAccount] }],
+//   }).then((result) => { res.json(result); });
+// });
 
 module.exports = router;
