6c0ddebf4cb9efcf4a8508978e2d1b2dd1e9d8bb
Dahyun Chung <honeybeat1@gmail.com>
2020-08-26 20:14:32 +0900

    [ADD] seeder 데이터 추가

---
+++ src/seeders/checkout-seeder.js
@@ -0,0 +1,36 @@
+module.exports = {
+  up: async (queryInterface) => queryInterface.bulkInsert('checkout', [{
+    due_date: '2020-08-26',
+    checkin_date: '2020-08-26',
+    checkout_status: 'no harm(dachung)',
+    checkin_status: 'no harm(ckang)',
+    book_tiger_id: 1,
+    user_account_id: 1,
+    created_at: '2020-8-21',
+    updated_at: '2020-8-21',
+  }, {
+    due_date: '2020-08-20',
+    checkin_date: '2020-08-26',
+    checkout_status: 'no harm(jbeen)',
+    checkin_status: 'yes damage(jbeen)',
+    book_tiger_id: 2,
+    user_account_id: 2,
+    created_at: '2020-8-21',
+    updated_at: '2020-8-21',
+  }, {
+    due_date: '2020-08-31',
+    checkout_status: 'no harm(jkang)',
+    book_tiger_id: 3,
+    user_account_id: 1,
+    created_at: '2020-8-21',
+    updated_at: '2020-8-21',
+  }, {
+    due_date: '2020-09-01',
+    checkout_status: 'yes damage(dachung)',
+    book_tiger_id: 4,
+    user_account_id: 2,
+    created_at: '2020-8-21',
+    updated_at: '2020-8-21',
+  }], {}),
+  down: async (queryInterface) => queryInterface.bulkDelete('checkout', null, {}),
+};
